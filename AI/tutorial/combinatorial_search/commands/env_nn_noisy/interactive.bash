e2982133087839a52e252d7d794ba75e63ff42d7
czxttkl <czxttkl@gmail.com>
2018-02-04 13:36:14 -0500

    add more commands

---
+++ AI/tutorial/combinatorial_search/commands/env_nn_noisy/interactive.bash
@@ -0,0 +1,15 @@
+python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t43200/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t43200/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t43200/optimizer_model_fixedxoFalse/qlearning"
+
+python3.6 experimenter.py --method="ga" --wall_time_limit=50 --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" \
+&& python3.6 experimenter.py --method="ga" --wall_time_limit=50 --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" \
+&& python3.6 experimenter.py --method="ga" --wall_time_limit=50 --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303"
+
+python3.6 experimenter.py --method="ga" --wall_time_limit=25 --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" \
+&& python3.6 experimenter.py --method="ga" --wall_time_limit=25 --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" \
+&& python3.6 experimenter.py --method="ga" --wall_time_limit=25 --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303"
+
+python3.6 experimenter.py --method="random" --wall_time_limit=50 --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" \
+&& python3.6 experimenter.py --method="random" --wall_time_limit=50 --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" \
+&& python3.6 experimenter.py --method="random" --wall_time_limit=50 --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303"
--- AI/tutorial/combinatorial_search/commands/env_nn_noisy/rlprtr_6.bash
+++ AI/tutorial/combinatorial_search/commands/env_nn_noisy/rlprtr_6.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=rlprtr
+#SBATCH --job-name=rlprtr_6
 #SBATCH --error=slurm/rlprtr_err
 #SBATCH --out=slurm/rlprtr_out
 #SBATCH --exclusive
@@ -12,4 +12,4 @@ source activate myenv
 work=/home/chen.zhe/combinatorial_search
 cd $work
 
-python -u Q_comb_search.py --env_name="env_nn_noisy" --k=200 --d=30 --test_period=100 --load=0 --env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --learn_wall_time_limit=43200 --root_dir="prtr_models" > slurm/rlprtr_out
+python -u Q_comb_search.py --env_name="env_nn_noisy" --k=200 --d=30 --test_period=100 --load=0 --env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --learn_wall_time_limit=21600 --root_dir="prtr_models" > slurm/rlprtr_out
