c02afe3916f7a64b2560a27ec50b68ed387e016e
czxttkl <czxttkl@gmail.com>
2018-02-03 22:05:12 -0500

    add commands on discovery

---
+++ AI/tutorial/combinatorial_search/commands/env_nn_noisy/prob_generate.bash
@@ -0,0 +1,3 @@
+python3.6 problem_generator.py --k=200 --d=30 --env=env_nn_noisy --pv=0 --env_seed=303
+python3.6 problem_generator.py --k=200 --d=30 --env=env_nn_noisy --pv=1 --env_seed=303
+python3.6 problem_generator.py --k=200 --d=30 --env=env_nn_noisy --pv=2 --env_seed=303
