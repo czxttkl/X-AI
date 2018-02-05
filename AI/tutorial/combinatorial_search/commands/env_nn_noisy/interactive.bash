198765107186bb5fa45506f336ee804e47c1f334
czxttkl <czxttkl@gmail.com>
2018-02-05 17:34:24 -0500

    add more commands

--- AI/tutorial/combinatorial_search/commands/env_nn_noisy/interactive.bash
+++ AI/tutorial/combinatorial_search/commands/env_nn_noisy/interactive.bash
@@ -1,3 +1,19 @@
+# 1 hr prtr
+python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t3600/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t3600/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t3600/optimizer_model_fixedxoFalse/qlearning"
+
+# 3 hr prtr
+python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t10800/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t10800/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t10800/optimizer_model_fixedxoFalse/qlearning"
+
+# 6 hr prtr
+python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t21600/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t21600/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t21600/optimizer_model_fixedxoFalse/qlearning"
+
+# 12 hr prtr
 python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv0_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t43200/optimizer_model_fixedxoFalse/qlearning" \
 && python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv1_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t43200/optimizer_model_fixedxoFalse/qlearning" \
 && python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_nn_noisy_pv2_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_nn_noisy_k200_d30_t43200/optimizer_model_fixedxoFalse/qlearning"
