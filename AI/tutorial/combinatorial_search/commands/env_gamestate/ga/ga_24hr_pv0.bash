408f791f746756a0854b424c926bb1930470748e
czxttkl <czxttkl@gmail.com>
2018-02-18 21:52:38 -0500

    add more commands

--- AI/tutorial/combinatorial_search/commands/env_gamestate/ga/ga_24hr_pv0.bash
+++ AI/tutorial/combinatorial_search/commands/env_gamestate/ga/ga_24hr_pv0.bash
@@ -12,7 +12,7 @@ source activate myenv
 work=/home/chen.zhe/combinatorial_search
 cd $work
 
-python -u experimenter.py --method="ga" --wall_time_limit=82800 --prob_env_dir="test_probs/prob_env_gamestate_pv0_envseed303" > slurm/ga_24hr_pv0_out
+python -u experimenter.py --method="ga" --wall_time_limit=80000 --prob_env_dir="test_probs/prob_env_gamestate_pv0_envseed303" > slurm/ga_24hr_pv0_out
 
 # replace pvx to pvy
 # replace ga_xx to ga_yy
---
+++ AI/tutorial/combinatorial_search/commands/env_gamestate/rlprtr/rlprtr_test_12day_interactive.bash
@@ -0,0 +1,13 @@
+# 24hr prtr
+python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv0_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv1_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv2_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv3_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv4_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv5_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv6_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv7_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv8_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+&& python3.6 experimenter.py --method="rl_prtr" --prob_env_dir="test_probs/prob_env_gamestate_pv9_envseed303" --prtr_model_dir="prtr_models/rl_prtr_env_gamestate_k312_d15_t2591000_12/optimizer_model_fixedxoFalse/qlearning" \
+
+
